e8fd7a3a6b46b8607da5c6c9179da1c578f939db
ManyataBhawsar <manyatabhawsar405@gmail.com>
2026-02-28 00:59:38 +0530

    Added project files

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/" ,(req , res)=>{
+    res.send("Hello! Server Running");
+});
+
+app.get("/api" ,(req,res)=>{
+    res.json({
+        message:"Success"
+    });
+});
+
+app.listen(PORT , ()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
